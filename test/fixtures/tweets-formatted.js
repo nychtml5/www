fdeace13c499e60853f10e34625db2861a876ea4
Trevor Landau <landautrevor@gmail.com>
2014-05-21 00:50:02 -0400

    simplify tweets

---
+++ test/fixtures/tweets-formatted.js
@@ -0,0 +1,311 @@
+'use strict';
+module.exports = [{
+  id: 'tweet21',
+  id_str: '456580391533424640',
+  text: '@brittanystorozforgiven(true)',
+  created_at: 'WedApr1623: 50: 22+00002014',
+  user: {
+    id: 'user22',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr16th2014@7: 50pm'
+},
+{
+  id: 'tweet39',
+  id_str: '444204867855609856',
+  text: 'RT@brittanystoroz: Somuchfunspeaking@NYCHTML5lastnight,
+  thanksforallthefeedbackeverybody.SeeyouallatthenextoneonApril…',
+  created_at: 'ThuMar1320: 14: 28+00002014',
+  user: {
+    id: 'user40',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Mar13th2014@4: 14pm'
+},
+{
+  id: 'tweet37',
+  id_str: '444560150935121920',
+  text: 'Wewerefeaturedina@jennschifferoriginalandwedidn’tevenknow!https: //t.co/sTdHbKVoa9',
+  created_at: 'FriMar1419: 46: 14+00002014',
+  user: {
+    id: 'user38',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Mar14th2014@3: 46pm'
+},
+{
+  id: 'tweet35',
+  id_str: '445642870327095297',
+  text: 'RT@joe_sepi: Our@NYCHTML5HackerHourseventthismonthisat@MakerStudios.Signupbeforeitfillsup!Onlyafewspotsleft.http: //t.…',
+  created_at: 'MonMar1719: 28: 34+00002014',
+  user: {
+    id: 'user36',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Mar17th2014@3: 28pm'
+},
+{
+  id: 'tweet9',
+  id_str: '459439408945446914',
+  text: '@djfarrellyButwhynot?Whattakesyouawayfromthiswonderfulcity?Justcancelthoseplans.',
+  created_at: 'ThuApr2421: 11: 05+00002014',
+  user: {
+    id: 'user10',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr24th2014@5: 11pm'
+},
+{
+  id: 'tweet33',
+  id_str: '447382521752584192',
+  text: '.@NYCHTML5hasaURLtomatchitsnamenow!http: //t.co/3prW1nvH85/cc@trevor_landau@joe_sepi',
+  created_at: 'SatMar2214: 41: 19+00002014',
+  user: {
+    id: 'user34',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Mar22nd2014@10: 41am'
+},
+{
+  id: 'tweet29',
+  id_str: '452523117714624512',
+  text: 'RT@denimf: Twoweeksfromnow,
+  I\'llbespeakingat@NYCHTML5meetupabout#html5clientsidestoragetechnologieshttp: //t.co/qLJRge0OgM',
+  created_at: 'SatApr0519: 08: 13+00002014',
+  user: {
+    id: 'user30',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr5th2014@3: 08pm'
+},
+{
+  id: 'tweet23',
+  id_str: '455719642497957889',
+  text: 'RT@denimf: .@NYCHTML5teaserfortheWednesdaypresentation-samplesworkinallbrowsers#clientstoragehttp: //t.co/XP5zn8qjHY',
+  created_at: 'MonApr1414: 50: 04+00002014',
+  user: {
+    id: 'user24',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr14th2014@10: 50am'
+},
+{
+  id: 'tweet17',
+  id_str: '456810468716580864',
+  text: 'RT@arkitrave: Thankstoallwhocameoutto@NYCHTML5lastnight,
+  andIappreciatedallthefeedbackonthetalk.I\'llkeepthehand-drawn…',
+  created_at: 'ThuApr1715: 04: 37+00002014',
+  user: {
+    id: 'user18',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr17th2014@11: 04am'
+},
+{
+  id: 'tweet19',
+  id_str: '456809088341393408',
+  text: 'RT@kathleenyano: HadadreamthatIforgottoregisterforthenext@NYCHTML5meetupanditwastotallyfull.Thankfullyitwasonlyadre…',
+  created_at: 'ThuApr1714: 59: 08+00002014',
+  user: {
+    id: 'user20',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr17th2014@10: 59am'
+},
+{
+  id: 'tweet31',
+  id_str: '448993956127576064',
+  text: 'Anothersuccessfulmeetup!Pizzawasgreat!Venuewasgreat!Hackingwasgreat!@NYCHTML5mustbegreat!Thanksforhosting@MakerStudios!',
+  created_at: 'ThuMar2701: 24: 35+00002014',
+  user: {
+    id: 'user32',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Mar26th2014@9: 24pm'
+},
+{
+  id: 'tweet7',
+  id_str: '461861332095365120',
+  text: 'RT@johnkpaul: I’mexcitedfor@EmpireJSnextweek!Lookingatthespeakerlineup,
+  5ofthespeakersfirstspoke@NYCHTML5thisyear.',
+  created_at: 'ThuMay0113: 34: 57+00002014',
+  user: {
+    id: 'user8',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'May1st2014@9: 34am'
+},
+{
+  id: 'tweet5',
+  id_str: '461922223419191296',
+  text: 'DouglasCrockford-TheBetterParts\nhttp: //t.co/r2vt2zbisZ',
+  created_at: 'ThuMay0117: 36: 54+00002014',
+  user: {
+    id: 'user6',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'May1st2014@1: 36pm'
+},
+{
+  id: 'tweet3',
+  id_str: '466775615916761089',
+  text: 'RT@tbuchok: thanksagainto@NYCHTML5forhavingme.asalways,
+  slidesarehere: http: //t.co/8gH7uKb0qyhttp: //t.co/cssfj2z0uS',
+  created_at: 'ThuMay1503: 02: 33+00002014',
+  user: {
+    id: 'user4',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'May14th2014@11: 02pm'
+},
+{
+  id: 'tweet1',
+  id_str: '466783695995932672',
+  text: 'RT@scottweinstein: Thanksforhavingme@NYCHTML5.Slidesfor"custom lint rules for JavaScript"http: //t.co/QjUvDnQFNJ',
+  created_at: 'ThuMay1503: 34: 40+00002014',
+  user: {
+    id: 'user2',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'May14th2014@11: 34pm'
+},
+{
+  id: 'tweet27',
+  id_str: '453544585667424<t_co>',
+  text: 'RT@johnkpaul: There’sonlyonespotleftforthenext@NYCHTML5meetup.Signupnow!http: //t.co/TZFGLuYKyh',
+  created_at: 'TueApr0814: 47: 10+00002014',
+  user: {
+    id: 'user28',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr8th2014@10: 47am'
+},
+{
+  id: 'tweet15',
+  id_str: '458542426719010816',
+  text: 'RT@rememberlenny: Slidesfortonightstalkarenowup!http: //t.co/51kSZaKxd8@NYCHTML5 #computervision#html5',
+  created_at: 'TueApr2209: 46: 48+00002014',
+  user: {
+    id: 'user16',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr22nd2014@5: 46am'
+},
+{
+  id: 'tweet13',
+  id_str: '458616979797331968',
+  text: 'RT@TedMielczarek: Lookslikewe\'llhitbug1,
+  000,
+  000inMozilla\'sbugzillathisweek.',
+  created_at: 'TueApr2214: 43: 03+00002014',
+  user: {
+    id: 'user14',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr22nd2014@10: 43am'
+},
+{
+  id: 'tweet25',
+  id_str: '453713316485926912',
+  text: 'RT@floydophone: whoa,
+  @connors\'shttp: //t.co/NwMoydEGR5actuallylookslikeios7',
+  created_at: 'WedApr0901: 57: 38+00002014',
+  user: {
+    id: 'user26',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr8th2014@9: 57pm'
+},
+{
+  id: 'tweet11',
+  id_str: '459025030559846400',
+  text: 'Cometo@NYCHTML5onMay14th!http: //t.co/Yx1VYhpwbD',
+  created_at: 'WedApr2317: 44: 30+00002014',
+  user: {
+    id: 'user12',
+    id_str: '2296757942',
+    name: 'NYCHTML5',
+    profile_image_url: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png',
+    screen_name: 'NYCHTML5',
+    image: 'http: //pbs.twimg.com/profile_images/435907027357810688/_duUlTgo_normal.png'
+  },
+  date: 'Apr23rd2014@1: 44pm'
+}
+];
+
