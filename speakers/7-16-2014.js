d88527fe80d196fd7b54759bdb96ae6c5a1dccf8
Trevor Landau <landautrevor@gmail.com>
2014-06-19 09:21:15 -0400

    next meetup

--- speakers/7-16-2014.js
+++ speakers/7-16-2014.js
@@ -1,7 +1,27 @@
 'use strict';
 
 exports.title = 'JS Debugging, Living with MediaQueries and Speed Up Thy Style';
-exports.date = new Date(2014, 6, 16, 18, 30);
-exports.url = 'http://www.meetup.com/nychtml5/events/189877642/';
+exports.date = new Date(2014, 6, 16, 16, 30);
+exports.url = 'http://www.meetup.com/nychtml5/events/183102422/';
 exports.speakers = [
+  {
+    title: 'JS Debugging',
+    name: 'Amjad Masad',
+    twitter: 'amasad',
+    desc: 'As the complexity of the apps we\'re building grows it becomes harder and harder to debug. Beyond your typical console.log and breakpoint insertion debugging I want to discuss some of the lesser known JS debugging techniques that has helped me debug some of the more elusive bugs I\'ve seen while working on Facebook.'
+  },
+  {
+    title: 'Living with MediaQueries',
+    name: 'Alex Lee',
+    twitter: 'Aintaer',
+    desc: 'Where most approaches to media queries relegate it to a CSS-only affair, coupling media queries to javascript managed views allows more divergent designs. At (Adobe) Behance, we grapple with mediaQuery, wrestle it into an event source, and use it to drive behavior-divergent pages.'
+  },
+  {
+    title: 'Speed Up Thy Style',
+    name: 'Shefali Pandey',
+    twitter: 'shefalipandey',
+    desc: 'As browsers evolve so do best practices for page performance. Some classics have endured like making fewer requests from CSS and writing concise semantic markup for faster rendering. And then there are some contentious performance grey areas like Data URIs (when to use? should they be used?), inline styles (are they faster than external styles? if yes, should one sacrifice readability for performance?), using SVGs for images, using CSS preprocessors (a battle between maintainability and performance?).'
+  }
 ];
+
+
