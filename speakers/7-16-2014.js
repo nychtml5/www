7d386efa08739539e01ed0381a75952cba19d3db
Trevor Landau <landautrevor@gmail.com>
2014-06-19 09:10:12 -0400

    tmp fix

---
+++ speakers/7-16-2014.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports.title = 'JS Debugging, Living with MediaQueries and Speed Up Thy Style';
+exports.date = new Date(2014, 6, 16, 18, 30);
+exports.url = 'http://www.meetup.com/nychtml5/events/189877642/';
+exports.speakers = [
+];
