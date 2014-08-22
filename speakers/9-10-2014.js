d2f83c97716ffe2fe620f5fb77ffda721bda7eff
John K. Paul <john@johnkpaul.com>
2014-08-22 08:38:37 -0400

    Add September meetup

---
+++ speakers/9-10-2014.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports.title = 'Problematic Chromatics, PEG.js and Bitcoin.js';
+exports.date = new Date(2014, 8, 10, 10, 30);
+exports.url = 'http://www.meetup.com/nychtml5/events/195356672/';
+exports.speakers = [
+  {
+    title: 'Problematic Chromatics: Making colorblind accessible webpages',
+    name: 'Phoebe Stierhoff',
+    twitter: 'TychosDrunkElk',
+    desc: 'We often use color as a visualization tool, but can we effectively use color while providing the best experience for everyone? We will discuss why color is important, and how we can use basket.js, sass, and html5 elements to make our webpages colorblind-friendly with minimal impact on future development. '
+  },
+  {
+    title: 'PEG.js',
+    name: 'Kacy James',
+    desc: "CoffeeScript, LESS, and other source-to-source compilers have made our jobs as developers substantially easier when writing front-end code. In this talk, we’ll be investigating why source-to-source compilers exist, as well as implementing our own simple compiler using PEG.js."
+  },
+  {
+    title: 'Bitcoin.js and the Blockchain: Web 3.0 is here',
+    name: 'Jeff Ward',
+    twitter: 'jeffscottward',
+    desc:"In this talk we will discuss what Bitcoin is, why people use it, what new possibilities it provides, what the Blockchain is, and what it means for our future."
+  }
+];
+
+
