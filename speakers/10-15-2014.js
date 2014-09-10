b793bd02e473721bf528f0eae71b5b5b8aa39097
John K. Paul <john@johnkpaul.com>
2014-09-10 19:41:26 -0400

    Add October 15th meetup

---
+++ speakers/10-15-2014.js
@@ -0,0 +1,28 @@
+'use strict';
+
+exports.title = 'Problematic Chromatics, PEG.js and Bitcoin.js';
+exports.date = new Date(2014, 9, 15, 10, 30);
+exports.url = 'http://www.meetup.com/nychtml5/events/202186082/';
+exports.speakers = [
+  {
+    title: 'Everybody likes Brisket',
+    name: 'Wayne Warner',
+    twitter: 'wawjr3d',
+    desc: 'Brisket is a framework for building single page web apps using isomorphic javascript. A Brisket app can be a traditional web site or a single page web application. But because it\'s dope, it can be both at the same time. Brisket provides the tools that you need to spend your time focusing on application logic rather than on "what environment is my code running in?".'
+  },
+  {
+    title: 'r.js: A love story',
+    name: 'Joel Kemp',
+    twitter: 'mrjoelkemp',
+    desc:'Whether you bundle your AMD codebase into a single or multiple bundles, r.js eventually becomes the bottleneck for build process performance.  \n In this talk, I\'ll discuss the growing pains we\'ve had at Bēhance with r.js and the tooling we\'ve built to make our JS builds smarter and lightning fast.'
+  },
+  {
+    title: 'Google Closure',
+    name: 'Erik Schmidt',
+    twitter: '3rikschmidt',
+    desc:"A talk about Google Closure."
+  }
+];
+
+
+
